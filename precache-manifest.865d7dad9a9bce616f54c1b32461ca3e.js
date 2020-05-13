1f4b12b97c140b72bbb5ed2a3447d7c423a6c7db
endayusma <enda.nur.a.y@mail.ugm.ac.id>
2020-05-13 14:53:29 +0700

    Updates

---
+++ precache-manifest.865d7dad9a9bce616f54c1b32461ca3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6702753cdf7f8c5809222dd4d86730",
+    "url": "/novelis_/index.html"
+  },
+  {
+    "revision": "f00e654ca2ce6de32983",
+    "url": "/novelis_/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "17fdd7a39f3f1cf19c10",
+    "url": "/novelis_/static/css/main.00d72222.chunk.css"
+  },
+  {
+    "revision": "f00e654ca2ce6de32983",
+    "url": "/novelis_/static/js/2.028b1056.chunk.js"
+  },
+  {
+    "revision": "494ff15427fc00bc6d2909d429ff11b9",
+    "url": "/novelis_/static/js/2.028b1056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17fdd7a39f3f1cf19c10",
+    "url": "/novelis_/static/js/main.1ebefd63.chunk.js"
+  },
+  {
+    "revision": "763f7b18577d5cae7393",
+    "url": "/novelis_/static/js/runtime-main.281af706.js"
+  }
+]);
